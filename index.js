10b04b4d27696be5e27e8f2776f119913182cb23
Terry Ouyang <terry.ouyang@gmail.com>
2020-08-25 23:30:03 +0800

    add: error prompting

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ var port = process.env.PORT || config.port || 9999;
 
 app.listen(port, null, function (err) {
   console.log('Gatekeeper, at your service: http://localhost:' + port);
+  if (err) {
+    console.error(err);
+  }
 });
